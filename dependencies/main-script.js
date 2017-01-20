008f50ca8977a1f7b43c5b7c94757856f1a291e4
Draklive <eriwes1ros@rosendal.local>
2017-01-20 13:03:15 +0100

    Fixed some things to improve code readability and increase simlicity

--- dependencies/main-script.js
+++ dependencies/main-script.js
@@ -13,43 +13,20 @@ function pageScroll()
 	var scrolltop = document.getElementById("page").scrollTop;
 	var percent = scrolltop/width;
 	
+	document.getElementById("menu-bar-fixed").innerHTML = document.getElementById("menu-bar").innerHTML;
+	
 	var element = document.getElementById("menu-bar");
 	if (percent > 0.145)
 	{
-		addClass(element, "menu-bar-fixed");
-		document.getElementsByClassName("menu-bar-fixed")[0].style.width = document.getElementById("page").clientWidth + "px";
-		document.getElementById("menu-bar-spacer").style.display = "block";
+		document.getElementById("menu-bar-fixed").style.width = document.getElementById("page").clientWidth + "px";
+		document.getElementById("menu-bar-fixed").style.display = "block";
 	}
 	else
 	{
-		removeClass(element, "menu-bar-fixed");
-		document.getElementById("menu-bar-spacer").style.display = "none";
+		document.getElementById("menu-bar-fixed").style.display = "none";
 	}
 }
 
 window.addEventListener('resize', function(event){
   document.getElementsByClassName("menu-bar-fixed")[0].style.width = document.getElementById("page").clientWidth + "px";
 });
-
-//others code, but it seems to work
-function hasClass(el, className) {
-  if (el.classList)
-    return el.classList.contains(className)
-  else
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
-}
-
-function addClass(el, className) {
-  if (el.classList)
-    el.classList.add(className)
-  else if (!hasClass(el, className)) el.className += " " + className
-}
-
-function removeClass(el, className) {
-  if (el.classList)
-    el.classList.remove(className)
-  else if (hasClass(el, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    el.className=el.className.replace(reg, ' ')
-  }
-}
